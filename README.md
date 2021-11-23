c79e3f5360a0dfe5dab368d5023ae5e80d92f69c
MatildaDelacourt <matilda.delacourt@gmail.com>
2021-11-22 23:25:04 -0500

    add resistor calculator

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Resistor
+# Resistor Calculator
+
+## Here we have a resistor calculator that calculates the resistance of the color bands.
+
+### To use the calculator you just need to select different colors and you get the answer.
--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 import { getResistorOhms } from './resistor.js';
+
+
+const color0element = document.getElementById("color0");
+const color1element = document.getElementById("color1");
+const color2element = document.getElementById("color2");
+const color3element = document.getElementById("color3");
+
+let color0 = 'red';
+let color1 = 'green';
+let color2 = 'violet';
+let color3 = 'gold';
+
+
+color0element.addEventListener('click', function(e) {
+    document.querySelector("#b0").className ="";
+    color0 = e.target.innerHTML;
+    document.querySelector("#b0").classList.add(color0);
+    document.querySelector("#b0").classList.add("band");
+    calculate();
+});
+
+color1element.addEventListener('click', function(e) {
+    document.querySelector("#b1").className ="";
+    color1 = e.target.innerHTML;
+    document.querySelector("#b1").classList.add(color1);
+    document.querySelector("#b1").classList.add("band");
+    calculate();
+});
+
+color2element.addEventListener('click', function(e) {
+    document.querySelector("#b2").className ="";
+    color2 = e.target.innerHTML;
+    document.querySelector("#b2").classList.add(color2);
+    document.querySelector("#b2").classList.add("band");
+    calculate();
+});
+
+color3element.addEventListener('click', function(e) {
+    document.querySelector("#b3").className ="";
+    color3 = e.target.innerHTML;
+    document.querySelector("#b3").classList.add(color3);
+    document.querySelector("#b3").classList.add("band");
+    calculate();
+});
+
+function calculate(){
+    let example =  {
+        color1: color0,
+        color2: color1,
+        multiplier: color2,
+        tolerance: color3
+      };
+ 
+    let answer = getResistorOhms(example);
+ 
+    console.log('answer is: ',answer);
+
+    document.getElementById('answer').innerText = answer;
+}
